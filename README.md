c71e00786c891442eff549c85c415ed2cc72dc8d
Obi Fortune <gabrielobi.of@gmail.com>
2022-02-12 11:39:54 +0200

    base server setup

---
+++ README.md
@@ -0,0 +1,1 @@
+# EmuAccom-BE
---
+++ index.js
@@ -0,0 +1,29 @@
+require("colors")
+const express = require("express")
+const dotenv = require("dotenv")
+const morgan = require("morgan")
+
+dotenv.config()
+
+const PORT = process.env.PORT || 5000
+const VERSION_NUMBER = process.env.VERSION_NUMBER || "v1"
+const NODE_ENV = process.env.NODE_ENV
+
+const app = express()
+
+if (NODE_ENV === "development") app.use(morgan("dev"))
+
+app.use(express.json())
+// app.use(`/${VERSION_NUMBER}/api`, require("./routes"))
+
+app.get("/", (req, res) => {
+  res.send("API is running....")
+})
+
+app.listen(
+  PORT,
+  console.log(
+    `\nServer running in ${NODE_ENV} mode on port ${PORT} -> http://localhost:${PORT}/${VERSION_NUMBER}/api`
+      .blue.bold,
+  ),
+)
