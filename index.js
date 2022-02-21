459a01ff2986809819bbac36934bc3255e947457
Obi Fortune <gabrielobi.of@gmail.com>
2022-02-21 22:26:34 +0200

    prettier updates

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-require("colors")
-require("dotenv").config()
+require("colors");
+require("dotenv").config();
 
-const express = require("express")
-const morgan = require("morgan")
+const express = require("express");
+const morgan = require("morgan");
 
-const PORT = process.env.PORT || 5000
-const VERSION_NUMBER = process.env.VERSION_NUMBER || "v1"
-const NODE_ENV = process.env.NODE_ENV
+const PORT = process.env.PORT || 5000;
+const VERSION_NUMBER = process.env.VERSION_NUMBER || "v1";
+const NODE_ENV = process.env.NODE_ENV;
 
-require("./DB").connectMONGO()
+require("./DB").connectMONGO();
 
-const app = express()
+const app = express();
 
-if (NODE_ENV === "development") app.use(morgan("dev"))
+if (NODE_ENV === "development") app.use(morgan("dev"));
 
-app.use(express.json())
+app.use(express.json());
 // app.use(`/${VERSION_NUMBER}/api`, require("./routes"))
 
 app.get("/", (req, res) => {
-  res.send("API is running....")
-})
+  res.send("API is running....");
+});
 
 app.listen(
   PORT,
@@ -27,4 +27,4 @@ app.listen(
     `\nServer running in ${NODE_ENV} mode on port ${PORT} -> http://localhost:${PORT}/${VERSION_NUMBER}/api`
       .blue.bold,
   ),
-)
+);
