2a09903da518dc9f26fa5cff750f1fd7904485bc
Obi Fortune <gabrielobi.of@gmail.com>
2022-03-26 13:24:14 +0200

    Updated MONGO connection for the railway deploy

--- DB/index.js
+++ DB/index.js
@@ -1,14 +1,15 @@
-const mongoose = require("mongoose")
-
+const mongoose = require("mongoose");
+const mongooseConfig = require("../configs/mongoose.config");
 exports.connectMONGO = async () => {
+  const mongoURL = process.env.MONGO_URL;
+  const mongoDB = process.env.NODE_ENV;
+  const URI = `${mongoURL}/${mongoDB}`;
+
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
-    console.log(`\nMongoDB Connected: ${conn.connection.host}`.cyan.underline)
+    const conn = await mongoose.connect(URI, mongooseConfig);
+    console.log(`\nMongoDB Connected: ${conn.connection.host}`.cyan.underline);
   } catch (error) {
-    console.error(`\nError: ${error.message}`.red.underline.bold)
-    process.exit(1)
+    console.error(`\nError: ${error.message}`.red.underline.bold);
+    process.exit(1);
   }
-}
+};
