b85d4a6b26950c3e347b6f33878f6b31a9e837fc
Obi Fortune <gabrielobi.of@gmail.com>
2022-04-13 08:46:43 +0300

    added object copying to duplicate the dorms for now

--- data/_mongo-seeder.js
+++ data/_mongo-seeder.js
@@ -16,10 +16,14 @@ const importData = async () => {
 
     const { dorms } = require("./dormTestData");
     if (dorms.length > 0 && users.length > 0) {
-      const reviewedDorms = dorms.map(dorm => {
+      const modifiedDorms = dorms.map(dorm => {
         return { ...dorm, management: createdUsers[0]._id };
       });
-      await Dormitory.insertMany(reviewedDorms);
+      // Only done to duplicate the dorms while waiting for real data
+      const duplicatedDorms = Array(40)
+        .fill(0)
+        .map(() => Object.assign({}, modifiedDorms[0]));
+      await Dormitory.insertMany(duplicatedDorms);
     }
 
     const { houses } = require("./houseTestData");
